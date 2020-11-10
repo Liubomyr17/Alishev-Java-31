927cf98dca5e0741de844ad84ea86d74f3c86d4d
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-11-10 20:34:46 +0200

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import javax.smartcardio.CardTerminal;
+
+public class Test {
+    public static void main(String[] args) {
+        int x= 1;
+        Integer x2 = 123;
+        int y = x2;
+
+        
+
+      //  Double, Float, Long, Integer, Short, Byte, Character, Boolean;
+    }
+}
